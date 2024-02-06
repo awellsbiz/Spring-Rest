49f3e05bde1fafbb881494359984c7ea010d7205
awellsbiz <awellsbiz@gmail.com>
2024-02-06 13:52:52 -0800

    commit

--- src/main/java/com/restapi/rest/RestApplication.java
+++ src/main/java/com/restapi/rest/RestApplication.java
@@ -3,7 +3,8 @@ package com.restapi.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // aliens for differend annotaions @Configurations @ ComponentScaan and
+												// @EnableAutoConfiguation
 public class RestApplication {
 
 	public static void main(String[] args) {
